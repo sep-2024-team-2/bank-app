4e399dd199784d02719becf6f0e178a44752fcd4
Uslje <147374020+uslje13@users.noreply.github.com>
2024-11-23 10:17:42 +0100

    feat: init app with db connectivity

---
+++ source/App.ts
@@ -0,0 +1,48 @@
+import express, { Application } from 'express';
+import http from "http";
+import sequelize from "./config/database";
+import { executeSqlScript } from "./utils/executeSqlScript";
+
+class App {
+    public app: Application;
+    public server: http.Server;
+
+    constructor() {
+        this.app = express();
+        this.server = http.createServer(this.app);
+    }
+
+    public async init() {
+        await this.connectToDatabase();
+        await this.executeInitialData();
+    }
+
+    private async connectToDatabase() {
+        try {
+            await sequelize.authenticate();
+            console.log("Database connection has been established successfully.");
+
+            await sequelize.sync({ alter: true });
+
+            console.log("All models were synchronized successfully.");
+        } catch (error) {
+            console.error("Unable to connect to the database:", error);
+        }
+    }
+
+    private async executeInitialData() {
+        try {
+            await executeSqlScript("data.sql");
+        } catch (error) {
+            console.error("Error executing initial data script:", error);
+        }
+    }
+
+    public start(port: number) {
+        this.server.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    }
+}
+
+export default App;
---
+++ source/config/database.ts
@@ -0,0 +1,18 @@
+import { Sequelize } from 'sequelize-typescript';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    ssl: false,
+    //models: [path.join(__dirname, '../models')],
+});
+
+export default sequelize;
